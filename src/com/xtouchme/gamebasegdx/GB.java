d4ce80810075ca3118fe4ba7f645ea2767d2a305
xTouchMe <jardsonsona@yandex.com>
2014-02-24 17:41:43 +0800

    Added distibution flags

--- src/com/xtouchme/gamebasegdx/GB.java
+++ src/com/xtouchme/gamebasegdx/GB.java
@@ -2,6 +2,10 @@ package com.xtouchme.gamebasegdx;
 
 public class GB {
 
-	public static boolean debug = false;
+	public enum Distribution {
+		DESKTOP, ANDROID, WEB
+	}
 	
+	public static boolean debug			= false;
+	public static Distribution distro	= Distribution.DESKTOP;
 }
